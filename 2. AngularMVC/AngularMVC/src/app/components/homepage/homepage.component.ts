810f5bf867dc66ef7f251957ad94985df785f510
Professor Wergeles <wergelesn@missouri.edu>
2020-10-13 16:51:51 -0500

    Code has bugs but this is the code we ended with after class on 10/13/2020

--- 2. AngularMVC/AngularMVC/src/app/components/homepage/homepage.component.ts
+++ 2. AngularMVC/AngularMVC/src/app/components/homepage/homepage.component.ts
@@ -2,13 +2,27 @@ import { Component, OnInit } from '@angular/core';
 
 import { PlanetModelService } from 'src/app/services/planet-model.service';
 
+import planets from '../../../assets/planetData.json';
+
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent implements OnInit {
+  selection;
+  formPlanetName;
+
+  views = ['Select One', 'list', 'table', 'system', 'all'];
+
+  data: Array<Planets> = planets;
+
   constructor(private model: PlanetModelService) {}
 
   ngOnInit(): void {}
+
+  selected() {
+    console.log(this.selection);
+    // this.views.push('test');
+  }
 }
